87739afd00670aae6c304e086c9f25fbb2595bc9
sahare92 <sahare92@gmail.com>
2019-09-08 14:10:39 +0300

    ...

--- pkg/processor/build/builder.go
+++ pkg/processor/build/builder.go
@@ -249,6 +249,7 @@ func (b *Builder) Build(options *platform.CreateFunctionBuildOptions) (*platform
 	}
 
 	if b.options.FunctionConfig.Spec.Image == "@set-after-build" {
+		enrichedConfiguration.Spec.Build.Path = ""
 		enrichedConfiguration.Spec.Image = processorImage
 	}
 
@@ -557,9 +558,6 @@ func (b *Builder) resolveFunctionPath(functionPath string) (string, error) {
 		if isURL {
 			b.options.FunctionConfig.Spec.Build.CodeEntryType = ArchiveEntryType
 		} else {
-
-			// this means function path is local - remove it and use the built image instead
-			b.options.FunctionConfig.Spec.Build.Path = ""
 			b.options.FunctionConfig.Spec.Build.CodeEntryType = ImageEntryType
 			b.options.FunctionConfig.Spec.Image = "@set-after-build"
 		}
