7b0ecc4af5bb1dc80fb32ae254e365ad57838f39
sahare92 <sahare92@gmail.com>
2020-07-10 09:35:47 +0300

    ...

--- pkg/platform/kube/controller/cronjobmonitoring.go
+++ pkg/platform/kube/controller/cronjobmonitoring.go
@@ -53,16 +53,33 @@ func (cjpd *CronJobMonitoring) startStaleCronJobPodsDeletionLoop() error {
 		// sleep until next deletion time staleCronJobPodsDeletionInterval
 		time.Sleep(*cjpd.staleCronJobPodsDeletionInterval)
 
-		err := cjpd.controller.kubeClientSet.
+		cronJobPods, err := cjpd.controller.kubeClientSet.
 			CoreV1().
 			Pods(cjpd.controller.namespace).
-			DeleteCollection(&metav1.DeleteOptions{},
-				metav1.ListOptions{
+			List(metav1.ListOptions{
 					LabelSelector: "nuclio.io/function-cron-job-pod=true",
 					FieldSelector: stalePodsFieldSelector,
 				})
 		if err != nil {
-			cjpd.logger.WarnWith("Failed to delete stale cron job pods", "err", err)
+			cjpd.logger.WarnWith("Failed to list stale cron job pods", "err", err)
+			continue
+		}
+
+		for _, cronJobPod := range cronJobPods.Items {
+
+			// if this pod is in pending for too long - delete it too
+			if cronJobPod.Status.Phase == v1.PodPending &&
+				cronJobPod.ObjectMeta.CreationTimestamp.After(time.Now().Add(-time.Minute)) {
+				continue
+			}
+
+			err := cjpd.controller.kubeClientSet.
+				CoreV1().
+				Pods(cronJobPod.Namespace).
+				Delete(cronJobPod.Name, &metav1.DeleteOptions{})
+			cjpd.logger.WarnWith("Failed to cleanup stale cron job pod",
+				"podName", cronJobPod.Name,
+				"err", err)
 		}
 	}
 }
@@ -72,7 +89,7 @@ func (cjpd *CronJobMonitoring) compileStalePodsFieldSelector() string {
 	var fieldSelectors []string
 
 	// filter out non stale pods by their phase
-	nonStalePodPhases := []v1.PodPhase{v1.PodPending, v1.PodRunning}
+	nonStalePodPhases := []v1.PodPhase{v1.PodRunning}
 	for _, nonStalePodPhase := range nonStalePodPhases {
 		selector := fmt.Sprintf("status.phase!=%s", string(nonStalePodPhase))
 		fieldSelectors = append(fieldSelectors, selector)
